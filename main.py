8a89c583960b68b459f1ea301163c112bf569225
Caden Helbling <caden.helbling@gmail.com>
2023-07-18 14:04:53 -0500

    Clean up main.py

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-# os.environ['PATH'] += r"/Users/chelblin/Drivers/Selenium/chromedriver_mac64/chromedriver"
 driver = webdriver.Chrome()
 driver.get("https://www.selenium.dev/")
 my_element = driver.find_element(By.XPATH, '//*[@id="main_navbar"]/ul/li[3]/a/span')
-# driver.implicitly_wait(3)
-# my_element.click()
-search_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="docsearch"]/button/span[1]/span'))
-)
-search_button.click()
-
-# Wait for the search box to become visible and interactable, then send 'Python'
-search_box = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="docsearch-input"]'))
-)
-search_box.send_keys('Python', Keys.ENTER)
-
-input("Press Enter to close the script and the web page.")
+driver.implicitly_wait(3)
+my_element.click()
+text = driver.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/p[1]')
+words = text.text
+print(words)
