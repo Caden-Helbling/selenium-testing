96e87ed70fb8fe56c0f8eee7e2b59d3205caad0b
Caden Helbling <caden.helbling@gmail.com>
2023-07-18 14:08:12 -0500

    Add comments to main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Set browser driver
 driver = webdriver.Chrome()
+
+# Load webpage
 driver.get("https://www.selenium.dev/")
+
+# Search for link to downloads page & wait for it to be clickable before clicking
 my_element = driver.find_element(By.XPATH, '//*[@id="main_navbar"]/ul/li[3]/a/span')
 driver.implicitly_wait(3)
 my_element.click()
+
+# Select a line of text and print it
 text = driver.find_element(By.XPATH, '/html/body/div/main/div[2]/div/div/p[1]')
 words = text.text
 print(words)
