43df42dca7b768440bbf5860aa50392cdd44f573
Caden Helbling <caden.helbling@gmail.com>
2023-07-28 16:29:28 -0500

    Import NoSuchElementException

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 options = Options()
 options.add_argument('--headless')
