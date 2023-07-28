e95a355cfe9190ce3d6095dcd16b661bc3f4edeb
Caden Helbling <caden.helbling@gmail.com>
2023-07-28 10:05:15 -0500

    Add error check for value retrieved

--- main.py
+++ main.py
@@ -22,8 +22,18 @@ password_input.send_keys(Keys.ENTER)
 driver.implicitly_wait(5)
 
 text = driver.find_element(By.XPATH, '//*[@id="app-container"]/div/div[2]/main/div[2]/div/p[1]')
-words = text.text
-print("Returned text: " + words)
+retrieved_text = text.text
+expected_text = "Returned text: Welcome to the U.S. GHG Center, your one-stop destination for all things related to greenhouse gas emissions! Our website offers a vast collection of datasets that are designed to help researchers, policymakers, and concerned citizens understand and mitigate the effects of climate change. Our team of experts has curated and compiled the most up-to-date and comprehensive data on greenhouse gas emissions from various sources, such as industry, transportation, and agriculture."
+
+print("Returned text: " + retrieved_text)
+
+if retrieved_text != expected_text:
+    driver.quit()
+    raise ValueError(f"The retrieved text '{retrieved_text}' does not match the expected value.")
+else:
+    print("Text matches the expected value.")
+
+driver.quit()
 
 # # Search for link to downloads page & wait for it to be clickable before clicking
 # my_element = driver.find_element(By.XPATH, '//*[@id="main_navbar"]/ul/li[3]/a/span')
