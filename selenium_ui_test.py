4be6e5a83c8325fc81533fb2ab5cf40686a8a6d6
Caden Helbling <caden.helbling@gmail.com>
2023-08-07 15:13:47 -0500

    print checkbox

--- selenium_ui_test.py
+++ selenium_ui_test.py
@@ -138,6 +138,7 @@ def perform_validation(dashboard_base_url):
     try:
         # Find the label element based on its attributes
         check_boxes = driver.find_element(By.XPATH, '//*[contains(@class, "input__FormInput-sc-52ed1q-0 ihwlly")]')
+        print(check_boxes)
 
         
         print("Check box element found on the webpage.")
