274adee182d3dfe3c2fa46343964cef6419c566c
Caden Helbling <caden.helbling@gmail.com>
2023-08-07 14:51:53 -0500

    Search for Heading

--- selenium_ui_test.py
+++ selenium_ui_test.py
@@ -135,7 +135,7 @@ def perform_validation(dashboard_base_url):
     action_menu_last10_year = driver.find_element(By.XPATH, '/html/body/div[10]/div/ul/li[4]/button')
     action_menu_last10_year.click()
 
-    heading = driver.find_element(By.XPATH, 'contains(text(), "heading")')
+    heading = driver.find_element(By.XPATH, 'contains(text(), "Heading")')
     print(heading)
 
     try:
