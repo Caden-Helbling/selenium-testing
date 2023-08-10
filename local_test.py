c85fc776ee6a812b346f5a686cf62803b6696a38
Caden Helbling <caden.helbling@gmail.com>
2023-08-10 14:44:59 -0500

    Update local test

--- local_test.py
+++ local_test.py
@@ -1,4 +1,5 @@
 import json
+import os
 import statistics
 import time
 from selenium import webdriver
@@ -8,54 +9,154 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 
-# PATH = "/Users/chelblin/Downloads/chromedriver-mac-arm64/chromedriver"
 options = Options()
-# options.add_argument('--headless')
-options.add_argument('--window-size=1920x1080')  # Set window size
-# options.add_experimental_option("detach", True)
+# options.add_argument('--headless') # Run browser in headless mode inside the github runner
+driver = webdriver.Chrome(options=options) # Set browser drive and pass options set above
+driver.set_window_size(1920,1080)
+driver.implicitly_wait(3) # Wait for element to load before throwing an error
 
-driver = webdriver.Chrome(options=options)
+# Load data from ui_data.json
+with open('ui_data.json') as json_file:
+    data = json.load(json_file)
 
-# Navigate to analysis page
-driver.get("https://deploy-preview-13--ghg-demo.netlify.app/analysis")
-driver.implicitly_wait(5) # Waits for the page to load
+class PageValidationException(Exception):
+    def __init__(self, mad_message=None, missing_logos=None, missing_catalogs=None, missing_datasets=None):
+        self.mad_message = mad_message
+        self.missing_logos = missing_logos
+        self.missing_catalogs = missing_catalogs
+        self.missing_datasets = missing_datasets
 
-print("UI_PASSWORD environment variable present. Entering password.")
-password_input = driver.find_element(By.XPATH, '//input[@name="password"]')
-password_input.send_keys("partnership")
-password_input.send_keys(Keys.ENTER)
+    def __str__(self):
+        message = "UI validation failed:\n"
 
-time.sleep(3)
+        if self.missing_logos:
+            message += "Missing logos:\n"
+            for logo in self.missing_logos:
+                message += f"  {logo}\n"
 
-with open("/Users/chelblin/repos/selenium-testing/page.html", "w", encoding='utf-8') as f:
-    f.write(driver.page_source)
-map_canvas = driver.find_element(By.XPATH, '//*[@class="mapboxgl-canvas"]')
-# driver.execute_script("arguments[0].scrollIntoView();", map_canvas)
+        if self.missing_catalogs:
+            message += "Missing catalogs:\n"
+            for catalog in self.missing_catalogs:
+                message += f"  {catalog}\n"
+        
+        if self.mad_message:
+            message += "Logos are out of alignment.\n"
 
-corner_coordinates = [
-    (-20, 20),
-    (60, 20),
-    (60, 60),
-    (-20, 60)
-]
+        if self.missing_datasets:
+            message += "Datasets are not appearing on analysis page.\n"
 
-actions = ActionChains(driver)
-for x, y in corner_coordinates:
-    actions.move_to_element_with_offset(map_canvas, x, y).click().perform()
+        return message
 
-map_canvas.send_keys(Keys.ENTER)
+def password_input():
+    print("UI_PASSWORD environment variable present. Entering password.")
+    password_input = driver.find_element(By.XPATH, '//input[@name="password"]')
+    password_input.send_keys(ui_password)
+    password_input.send_keys(Keys.ENTER)
 
-action_button = driver.find_element(By.XPATH, '//span[contains(text(), "Actions")]/following::button[contains(@class, "StyledButton")]')
-driver.execute_script("arguments[0].click();", action_button)
+def logo_validation(dashboard_base_url):
+    # dashboard_base_url = dashboard_base_url.rstrip('/') # remove the tailing /
+    driver.get(dashboard_base_url) # Load webpage "https://deploy-preview-13--ghg-demo.netlify.app/")
 
+    # Check whether a ui_password has been provided and enter it if required
+    if ui_password:
+        password_input()
 
-driver.find_element(By.XPATH, '//li//button[contains(text(), "Last 10 years")]').click()
+    # Check main page for logos containing source match in ui_data.json
+    logo_src_list = data["logos"]
+    missing_logos = []
+    y_coordinates = []
 
-try:
-    check_box = driver.find_element(By.XPATH, '//*[contains(@class, "checkable__FormCheckableText")]')
-    print("Check box element found on the webpage.")
+    for src in logo_src_list:
+        src = src.split("/")[-1].split(".")[0]
+        image_elements = driver.find_elements(By.XPATH, f"//img[contains(@src, '{src}')]")
+        if not image_elements:
+            missing_logos.append(src)
+        else:
+            for image_element in image_elements:
+                image_element_y = image_element.location['y']
+                y_coordinates.append(image_element_y)
 
-except NoSuchElementException:
-    print("Label element not found on the webpage.")
+    # Calculate the mean absolute deviation (MAD) of logo y positions
+    mean_y = statistics.mean(y_coordinates)
+    absolute_deviations = [abs(y - mean_y) for y in y_coordinates]
+    mad = statistics.mean(absolute_deviations)
+    if mad > 13:
+        mad_message = True
+        raise PageValidationException(mad_message=mad_message)
 
-    
+def catalog_verification(dashboard_base_url):
+    # Check the catalog page for catalogs matching those in ui_test.json
+    driver.get(f"{dashboard_base_url}/data-catalog")
+
+    catalog_list = data["catalogs"]
+    missing_catalogs = []
+
+    for catalog in catalog_list:
+        try:
+            driver.find_element(By.XPATH, f'//h3[contains(text(), "{catalog}")]')
+        except NoSuchElementException:
+            missing_catalogs.append(catalog)
+            raise PageValidationException(missing_catalogs=missing_catalogs)
+
+def dataset_verification(dashboard_base_url):
+    # Check the analysis page for datasets
+    driver.get(f"{dashboard_base_url}/analysis")
+    time.sleep(3) # Give time for map to fully load and be clickable
+    map_canvas = driver.find_element(By.XPATH, '//*[@class="mapboxgl-canvas"]')
+
+    # Generate coordinates for corners of the rectangle to be drawn on the map
+    corner_coordinates = [
+        (-20, 20),
+        (60, 20),
+        (60, 60),
+        (-20, 60)
+    ]
+
+    # Click to create the rectangle one the map
+    actions = ActionChains(driver)
+    for x, y in corner_coordinates:
+        actions.move_to_element_with_offset(map_canvas, x, y).click().perform()
+
+    # Press enter after drawing map to signal completion of polygon
+    map_canvas.send_keys(Keys.ENTER)
+
+    # Expand the actions drop down and select 10 years
+    action_button = driver.find_element(By.XPATH, '//span[contains(text(), "Actions")]/following::button[contains(@class, "StyledButton")]')
+    driver.execute_script("arguments[0].click();", action_button)
+    driver.find_element(By.XPATH, '//li//button[contains(text(), "Last 10 years")]').click()
+
+    # Check that datasets exist
+    missing_datasets = False
+    try:
+        driver.find_element(By.XPATH, '//*[contains(@class, "checkable__FormCheckableText")]')
+    except NoSuchElementException:
+        raise PageValidationException(missing_datasets=missing_datasets)
+
+    # Raise exception if any validation fails
+    if missing_logos or missing_catalogs or mad_message or missing_datasets:
+        raise PageValidationException(missing_logos=missing_logos, missing_catalogs=missing_catalogs, mad_message=mad_message, missing_datasets=missing_datasets)
+    else:
+        print("Validation successful. All elements are present.")
+
+# Close the browser
+driver.quit()
+
+# Retry loop
+max_retries = 3
+dashboard_base_url = "https://deploy-preview-13--ghg-demo.netlify.app/"
+ui_password = "partnership"
+
+for retry in range(max_retries):
+    try:
+        logo_validation(dashboard_base_url)
+        catalog_verification(dashboard_base_url)
+        dataset_verification(dashboard_base_url)
+        break  # If validation is successful, break out of the loop
+    except PageValidationException as e:
+        if retry < max_retries - 1:
+            print(e)
+            print("Validation failed. Retrying...\n")
+            continue
+        else:
+            # Max retries reached, raise the exception again
+            raise e
