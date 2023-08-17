7337d8e06c87727c4376eeb45c2ac0d95872f8d2
Caden Helbling <caden.helbling@gmail.com>
2023-08-17 09:53:32 -0500

    Stop printing error on each run

--- selenium_ui_test.py
+++ selenium_ui_test.py
@@ -189,7 +189,7 @@ for retry in range(max_retries):
         break  # If validation is successful, break out of the loop
     except PageValidationException as e:
         if retry < max_retries - 1:
-            print(e)
+            # print(e)
             print("Validation failed. Retrying...")
             continue
         else:
